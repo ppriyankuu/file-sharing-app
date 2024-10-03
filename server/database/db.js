b7162fabf41e6367f493128fcb735f380b2f8b90
Priyanku Gogoi <114563767+ppriyankuu@users.noreply.github.com>
2024-10-03 19:25:51 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -13,8 +13,8 @@ const DBConnection = async () => {
   try {
     await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
     console.log("Database connected successfully");
-  } catch (error) {
-    console.log("Error while connecting with the database ", error.message);
+  } catch (err) {
+    console.log("Error while connecting with the database ", err.message);
   }
 };
 
